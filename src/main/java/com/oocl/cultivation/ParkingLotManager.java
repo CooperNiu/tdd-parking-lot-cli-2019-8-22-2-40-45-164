f98021a12150dfb23524d4889170c39afee108ab
CooperNiu <niuyanno.1@outlook.com>
2019-08-25 16:10:25 +0800

    stroy 5

---
+++ src/main/java/com/oocl/cultivation/ParkingLotManager.java
@@ -0,0 +1,32 @@
+package com.oocl.cultivation;
+
+import javax.crypto.interfaces.PBEKey;
+import java.util.ArrayList;
+
+public class ParkingLotManager {
+    ArrayList<ParkingBoy> parkingBoys;
+
+    public ParkingLotManager(ArrayList<ParkingLot> parkingLots, ArrayList<ParkingBoy> parkingBoys) {
+//        super(parkingLots);
+        this.parkingBoys = parkingBoys;
+    }
+
+    public ParkingTicket tellParkingBoy2Park(ParkingBoy parkingBoy, Car car) {
+        if(checkIfParkingBoyUnderCmd(parkingBoy))
+        {
+            return parkingBoy.park(car);
+        }
+        else {
+            return null;
+        }
+    }
+
+    public Car tellParkingBoy2FetchCar(ParkingBoy parkingBoy, ParkingTicket parkingTicket){
+        return parkingBoy.fetch(parkingTicket);
+    }
+
+    private boolean checkIfParkingBoyUnderCmd(ParkingBoy parkingBoy)
+    {
+        return this.parkingBoys.contains(parkingBoy);
+    }
+}
