632343eb818925173ea300f0e497b2598de846ec
CooperNiu <niuyanno.1@outlook.com>
2019-08-25 09:53:57 +0800

    car, parkinglot

--- src/main/java/com/oocl/cultivation/Car.java
+++ src/main/java/com/oocl/cultivation/Car.java
@@ -1,4 +1,7 @@
 package com.oocl.cultivation;
 
 public class Car {
+	public Car() {
+		
+	}
 }
--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -18,4 +18,16 @@ public class ParkingLot {
     public int getAvailableParkingPosition() {
         return cars.size() - capacity;
     }
+
+	@SuppressWarnings("unchecked")
+	public ParkingTicket parkCar(Car car) {
+		ParkingTicket parkingTicket = new ParkingTicket();
+		this.cars = (Map<ParkingTicket, Car>) cars.put(parkingTicket, car);
+		return parkingTicket;
+	}
+
+	public Car pickCar(ParkingTicket parkingTicket) {
+		// TODO Auto-generated method stub
+		return new Car();
+	}
 }
