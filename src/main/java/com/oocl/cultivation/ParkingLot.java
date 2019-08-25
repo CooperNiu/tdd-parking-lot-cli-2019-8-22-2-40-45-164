ffe2da8157d05ad00b9eaa8494542fd10f8c7f02
CooperNiu <niuyanno.1@outlook.com>
2019-08-25 10:38:28 +0800

    AC4, AC5

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -1,11 +1,16 @@
 package com.oocl.cultivation;
 
+import gherkin.lexer.Pa;
+import sun.security.krb5.internal.Ticket;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class ParkingLot {
     private final int capacity;
     private Map<ParkingTicket, Car> cars = new HashMap<>();
+    private static AtomicLong idCounter = new AtomicLong();
 
     public ParkingLot() {
         this(10);
@@ -15,19 +20,33 @@ public class ParkingLot {
         this.capacity = capacity;
     }
 
-    public int getAvailableParkingPosition() {
-        return cars.size() - capacity;
+    public ParkingTicket storeCar(Car car){
+        ParkingTicket parkingTicket = new ParkingTicket(createTicketID());
+        cars.put(parkingTicket, car);
+        return parkingTicket;
     }
 
-	@SuppressWarnings("unchecked")
-	public ParkingTicket parkCar(Car car) {
-		ParkingTicket parkingTicket = new ParkingTicket();
-		this.cars = (Map<ParkingTicket, Car>) cars.put(parkingTicket, car);
-		return parkingTicket;
-	}
+    public Car pickCar(ParkingTicket ticket){
+        cars.remove(ticket);
+        return cars.get(ticket);
+    }
 
-	public Car pickCar(ParkingTicket parkingTicket) {
-		// TODO Auto-generated method stub
-		return new Car();
-	}
+    public boolean isParkingLotHasCar(Ticket ticket){
+        return cars.containsKey(ticket);
+    }
+
+    private static String createTicketID()
+    {
+        String prefix = "T-";
+        String id = String.valueOf(idCounter.getAndIncrement());
+        id = String.format("%6s", id).replace(' ', '0');
+        String ticketID = prefix + id;
+        return ticketID;
+    }
+
+    public int getAvailablePosition() {
+        return cars.size() - capacity;
+        return capacity - cars.size();
+    }
 }
+
--- src/main/java/com/oocl/cultivation/ParkingTicket.java
+++ src/main/java/com/oocl/cultivation/ParkingTicket.java
@@ -1,4 +1,17 @@
 package com.oocl.cultivation;
 
 public class ParkingTicket {
+    private String ticketID;
+
+    public ParkingTicket(){
+
+    }
+
+    public ParkingTicket(String ticketID){
+        this.ticketID = ticketID;
+    }
+
+    public ParkingTicket generateTicket(){
+        return this;
+    }
 }
